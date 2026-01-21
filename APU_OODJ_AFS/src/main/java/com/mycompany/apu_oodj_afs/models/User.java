1776da423c77842885b3d59fc559497bab280b98
mcnellylisette <mcnellylisette3@gmail.com>
2026-01-21 13:53:12 +0800

    Update User

---
+++ APU_OODJ_AFS/src/main/java/com/mycompany/apu_oodj_afs/models/User.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.apu_oodj_afs.models;
+
+/**
+ *
+ * @author jamesmcnellylisette
+ */
+public class User {
+    
+}
